4692fac33f8fa0c371a741ab7aec67622d9d7a3f
Vrinda Dixit <dixitvrinda1704@gmail.com>
2026-03-10 18:47:43 +0530

    route change in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/works/:slug" element={<ProjectPage />} />
       </Routes>
     </BrowserRouter>
