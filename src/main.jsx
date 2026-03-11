53aeeff535b811518e2f5f1bb1bb9c78cd911f6a
Vrinda Dixit <dixitvrinda1704@gmail.com>
2026-03-11 22:46:09 +0530

    toggle button

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './theme.css'
+import { ThemeProvider } from './context/ThemeContext'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
