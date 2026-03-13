6c3eb3239cebda3a7e0fbd3bf98b325c44123ba3
Vrinda Dixit <dixitvrinda1704@gmail.com>
2026-03-13 23:08:33 +0530

    dark mode

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './theme.css'
-import { ThemeProvider } from './context/ThemeContext'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
